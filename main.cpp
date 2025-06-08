a9f9e395fe316abc061caa0d9c2260e3f26d283a
Dr. Eric O. Flores <eric@florestoro.com>
2025-06-07 19:38:47 -0600

    Update main.cpp
    Update the main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
+
     GtkWidget *window = create_pdf_viewer_window();
-    gtk_widget_show_all(window);
+    gtk_widget_show_all(window);  // ✅ Required to make all widgets visible
+
     gtk_main();
     return 0;
 }
